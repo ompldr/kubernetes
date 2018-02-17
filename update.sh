13f0c8fec69ebaacea291dc49ea0ef39a39abb81
Brenden Matthews <brenden@diddyinc.com>
2018-02-17 14:04:07 -0500

    Adjust formatting.

--- update.sh
+++ update.sh
@@ -4,6 +4,14 @@
 #  gke_ompldr_us-east4-a_ompldr-us-east4
 #  gke_ompldr_us-west1-a_ompldr-us-west1
 
+kubectl --context=gke_ompldr_us-east4-a_ompldr-us-east4 apply -f us-east4-macaroons.yaml
+kubectl --context=gke_ompldr_us-east4-a_ompldr-us-east4 apply -f lnd-us-east4.yaml
+kubectl --context=gke_ompldr_us-east4-a_ompldr-us-east4 apply -f bitcoind-us-east4.yaml
+
+kubectl --context=gke_ompldr_us-west1-a_ompldr-us-west1 apply -f us-west1-macaroons.yaml
+kubectl --context=gke_ompldr_us-west1-a_ompldr-us-west1 apply -f lnd-us-west1.yaml
+kubectl --context=gke_ompldr_us-west1-a_ompldr-us-west1 apply -f bitcoind-us-west1.yaml
+
 for ctx in $(kubectl config get-contexts --kubeconfig=./zpkubeconfig -o name); do
   kubectl --context="${ctx}" apply -f ssl-certs.yaml
   kubectl --context="${ctx}" apply -f secrets.yaml
